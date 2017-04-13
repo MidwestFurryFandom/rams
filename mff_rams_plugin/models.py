4863960268e1e962334e9690053a62938d79b916
Victoria Earl <kitsuta@gmail.com>
2017-04-13 10:47:25 -0400

    Add Power option for dealers As per https://github.com/MidwestFurryFandom/rams/issues/17, we now offer dealer applicants the choice of power or no power at their table and charge them appropriately. The price of power is configurable.

--- mff_rams_plugin/models.py
+++ mff_rams_plugin/models.py
@@ -1,5 +1,13 @@
 from . import *
 
 @Session.model_mixin
+class Group:
+    power = Column(Integer, default=0)
+
+    @cost_property
+    def power_cost(self):
+        return c.POWER_PRICE * self.power
+
+@Session.model_mixin
 class Attendee:
     comped_reason = Column(UnicodeText, default='', admin_only=True)
