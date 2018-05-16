84083fa5f854c96fd9f09443c361ceeaaac49de2
Victoria Earl <kitsuta@gmail.com>
2018-05-16 11:33:32 -0400

    Fix base-admin and additional imports

--- mff_rams_plugin/__init__.py
+++ mff_rams_plugin/__init__.py
@@ -3,6 +3,9 @@ from os.path import join
 from uber.jinja import template_overrides
 from uber.utils import mount_site_sections, static_overrides
 from .config import config
+from . import models  # noqa: F401
+from . import model_checks  # noqa: F401
+from . import automated_emails  # noqa: F401
 
 
 static_overrides(join(config['module_root'], 'static'))
