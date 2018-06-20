e1c3857b4658c74e60b520658503f1da144fe4ee
Jacob M. Dawson <dawson@vt.edu>
2018-06-20 12:18:41 -0500

    Added an email for early dealers being waitlisted

--- mff_rams_plugin/automated_emails.py
+++ mff_rams_plugin/automated_emails.py
@@ -1,6 +1,6 @@
 from uber.config import c
 from uber.automated_emails import MarketplaceEmailFixture
-from uber.utils import days_before
+from uber.utils import before,days_before
 
 
 MarketplaceEmailFixture(
@@ -16,3 +16,10 @@ MarketplaceEmailFixture(
     lambda g: g.status == c.APPROVED and days_before(2, g.dealer_payment_due)() and g.is_unpaid,
     needs_approval=False,
     ident='dealer_reg_payment_reminder_last_chance_mff')
+
+MarketplaceEmailFixture(
+    'Your {EVENT_NAME} ({EVENT_DATE}) dealer application has been waitlisted',
+    'dealers/pending_waitlisted.txt',
+    lambda g: g.status == c.WAITLISTED and g.registered < c.DEALER_REG_DEADLINE,
+    when=before(c.DEALER_WAITLIST_CLOSED),
+    ident='dealer_pending_now_waitlisted_mff')
