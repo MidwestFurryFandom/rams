ccc5effa9643cd5f77367d617fd12e976e8f04fa
Mark Murnane <mark@hackafe.net>
2024-07-23 16:24:21 -1000

    Adding public version of spin_rest_utils

--- uber/payments.py
+++ uber/payments.py
@@ -1,6 +1,6 @@
 import checkdigit.verhoeff as verhoeff
 import pytz
-from spin_rest_utils import utils as spin_rest_utils
+import spin_rest_utils
 from typing import Iterable
 from collections import OrderedDict
 from datetime import datetime, timedelta
