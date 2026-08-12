8119c11567f78653c1f77149d3d310677ea52ec3
Victoria Earl <kitsuta@gmail.com>
2026-08-12 20:53:10 +0200

    Remove import lines entirely

--- uber/site_sections/schedule.py
+++ uber/site_sections/schedule.py
@@ -1,5 +1,4 @@
 import json
-#import ics
 import pytz
 import cherrypy
 
--- uber/site_sections/staffing.py
+++ uber/site_sections/staffing.py
@@ -1,7 +1,6 @@
 import cherrypy
 from datetime import datetime, timedelta
-from pockets.autolog import log
-#import ics
+from pockets.autolog import logimport ics
 from sqlalchemy.orm import joinedload
 from sqlalchemy.orm.exc import NoResultFound
 
